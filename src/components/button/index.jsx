134555a74a2e6ef31eda86ad4b23fb210e4a9431
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2023-09-19 06:24:35 -0300

    feat: Add new button styles and functionality
    - Added new CSS classes for link buttons and disabled buttons
    - Updated Button component to handle link buttons, disabled state, and icon rendering
    - Wrapped button element with ClickEffect component for click animation

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,21 +1,29 @@
 import PropTypes from 'prop-types';
 import './button.css';
 import Icon from '../icon';
+import ClickEffect from '../clickEffect';
 
-const Button = ({ text, isOutline }) => {
-	const className = isOutline ? 'button button--outline' : 'button';
+const Button = ({ text, isOutline, isLink, iconName, disabled }) => {
+	let className = isOutline ? 'button button--outline' : 'button';
+	className = isLink ? 'button button--link' : className;
+	className = disabled ? 'button button--disabled' : className;
 
 	return (
-		<button className={className}>
-			<Icon iconName="exit" />
-			{text}
-		</button>
+		<ClickEffect>
+			<button className={className} disabled={disabled}>
+				{iconName && <Icon iconName={iconName} />}
+				{text}
+			</button>
+		</ClickEffect>
 	);
 };
 
 Button.propTypes = {
 	text: PropTypes.string.isRequired,
 	isOutline: PropTypes.bool,
+	isLink: PropTypes.bool,
+	iconName: PropTypes.string,
+	disabled: PropTypes.bool,
 };
 
 export default Button;
