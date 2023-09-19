dec4f76677a0cbc0caaa5c54449c16b95b08c028
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2023-09-19 06:24:42 -0300

    feat: Update header component
    - Change "Entrar" button text to "Sair"
    - Add "exit" icon to the "Sair" button
    - Disable the "Sair" button

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -16,7 +16,7 @@ const Header = () => (
 			</div>
 			<div className="buttons-container">
 				<Button text="Instruções de preparo" isOutline />
-				<Button text="Entrar" />
+				<Button text="Sair" iconName="exit" isLink disabled />
 			</div>
 		</Container>
 	</header>
