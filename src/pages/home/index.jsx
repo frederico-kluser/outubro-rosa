fd317b360a0a7859f5b9b8332a8beee65c1a7890
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2023-09-21 07:47:06 -0300

    feat: Add email and password input fields
    - Added `Container` and `Input` components
    - Implemented state hooks for email and password inputs
    - Updated JSX structure to include new components and input fields

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,12 +1,37 @@
 import './home.css';
 import Header from '../../components/header';
 import Footer from '../../components/footer';
+import Container from '../../components/container';
+import Input from '../../components/input';
+import { useState } from 'react';
 
-const Home = () => (
-	<div>
-		<Header />
-		<Footer />
-	</div>
-);
+const Home = () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	return (
+		<div>
+			<Header />
+			<section className="body">
+				<Container>
+					<div className="column-size">
+						<h1>Outubro Rosa</h1>
+						<div className="mt-32" />
+						<div className="container-16">
+							<p>Vamos começar com o e-mail e a senha</p>
+							<div className="input-container">
+								<Input caption="exemplo@nome.com" placeholder="E-mail" setValue={setEmail} type="email" value={email} />
+								<Input placeholder="Senha" setValue={setPassword} type="password" value={password} />
+							</div>
+							<button className="button">Entrar</button>
+						</div>
+					</div>
+					<div></div>
+				</Container>
+			</section>
+			<Footer />
+		</div>
+	);
+};
 
 export default Home;
