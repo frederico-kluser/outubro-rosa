bd9f607613fddcf769aeeb0f62432c42df714c64
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2023-09-22 10:38:23 -0300

    feat: Add responsive styling to home page
    - Added new CSS file for responsive styles
    - Imported and applied responsive styles to the home page template component

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react';
 import OutubroRosaPromo from '../../assets/outubro-rosa-promo.jpeg';
-import Header from '../../components/header';
-import Footer from '../../components/footer';
-import Container from '../../components/container';
 import Input from '../../components/input';
 import Button from '../../components/button';
-import './home.css';
 import Warning from '../../components/warning';
+import './home.css';
+import './home-responsive.css';
+import Template from '../../components/template';
 
 const Home = () => {
 	const [email, setEmail] = useState('');
@@ -14,46 +13,40 @@ const Home = () => {
 	const [error] = useState(false);
 
 	return (
-		<div>
-			<Header />
-			<section className="body">
-				<Container>
-					<div className="column-size">
-						<h1>Outubro Rosa</h1>
-						{error ? (
-							<>
-								<div className="mt-24" />
-								<Warning text="Dados incorretos. Por favor, tente de novo" />
-								<div className="mt-16" />
-							</>
-						) : (
-							<div className="mt-32" />
-						)}
-						<div className="container-16">
-							<p>Vamos começar com o e-mail e a senha</p>
-							<div className="input-container">
-								<Input caption="exemplo@nome.com" placeholder="E-mail" setValue={setEmail} type="email" value={email} />
-								<Input placeholder="Senha" setValue={setPassword} type="password" value={password} />
-							</div>
-							<Button text="Entrar" isCondensed />
-						</div>
+		<Template>
+			<div className="column-size">
+				<h1>Outubro Rosa</h1>
+				{error ? (
+					<>
+						<div className="mt-24" />
+						<Warning text="Dados incorretos. Por favor, tente de novo" />
+						<div className="mt-16" />
+					</>
+				) : (
+					<div className="mt-32" />
+				)}
+				<div className="container-16">
+					<p>Vamos começar com o e-mail e a senha</p>
+					<div className="input-container">
+						<Input caption="exemplo@nome.com" placeholder="E-mail" setValue={setEmail} type="email" value={email} />
+						<Input placeholder="Senha" setValue={setPassword} type="password" value={password} />
 					</div>
-					<div className="column-size reverse">
-						<div className="container-promo">
-							<div className="background"></div>
-							<div
-								className="image"
-								style={{
-									backgroundImage: `url(${OutubroRosaPromo})`,
-								}}
-							></div>
-							{/* <img src={OutubroRosaPromo} alt="Outubro Rosa Promotion Image" /> */}
-						</div>
-					</div>
-				</Container>
-			</section>
-			<Footer />
-		</div>
+					<Button text="Entrar" isCondensed />
+				</div>
+			</div>
+			<div className="column-size reverse">
+				<div className="container-promo">
+					<div className="background"></div>
+					<div
+						className="image"
+						style={{
+							backgroundImage: `url(${OutubroRosaPromo})`,
+						}}
+					></div>
+					{/* <img src={OutubroRosaPromo} alt="Outubro Rosa Promotion Image" /> */}
+				</div>
+			</div>
+		</Template>
 	);
 };
 
