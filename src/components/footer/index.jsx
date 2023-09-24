f22d8e14f421534c762c47f025d7ab5564afbb7f
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2023-09-24 19:52:04 -0300

    feat: Add responsive styling to footer
    - Added media query in footer-response.css for screens with a maximum width of 768px
    - Adjusted padding and flex-direction properties in the .fleury-footer class
    - Updated background-color property in footer.css to #ffffff
    - Modified text content and added new classes for desktop and mobile versions in index.jsx

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,13 +1,18 @@
 import FleuryLogo from '../../assets/grupo-fleury-logo.svg';
 import Container from '../container';
 import './footer.css';
+import './footer-response.css';
 
 const Footer = () => (
 	<footer className="fleury-footer">
 		<Container>
 			<img alt="Grupo Fleury" className="image-fleury-logo clickEffect" src={FleuryLogo} />
-			<p className="fleury-footer-text">
+			<p className="fleury-footer-text desktop">
 				Feito com ❤️ por Grupo Fleury - Todos os Direitos Reservados | <b>Política de Privacidade</b> | © 2023
+			</p>
+			<p className="fleury-footer-text mobile">Feito com ❤️ por Grupo Fleury - Todos os Direitos</p>
+			<p className="fleury-footer-text mobile">
+				Reservados | <b>Política de Privacidade</b> | © 2023
 			</p>
 		</Container>
 	</footer>
