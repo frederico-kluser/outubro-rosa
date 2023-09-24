73cd38c09e60633bda5d15648c4ab25c448c4c5d
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2023-09-24 19:52:10 -0300

    feat(header): Add mobile menu functionality
    - Added CSS styles for the mobile menu
    - Implemented state and event handling to toggle the opening and closing of the mobile menu
    - Updated JSX structure to include the mobile menu components

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,31 +1,59 @@
 import FleuryLogo from '../../assets/grupo-fleury-logo.svg';
 import OutubroRosaLogo from '../../assets/outubro-rosa-logo.png';
 import MenuIcon from '../../assets/menu.svg';
+import ExitIcon from '../../assets/exit-icon.svg';
+import FileIcon from '../../assets/file.svg';
 import Button from '../button';
 import Container from '../container';
 import './header.css';
 import './header-responsive.css';
+import { useState } from 'react';
 
-const Header = () => (
-	<header className="fleury-header">
-		<Container>
-			<div className="logos-container">
-				<div className="logo-container">
-					<img alt="Grupo Fleury" className="image-fleury-logo clickEffect" src={FleuryLogo} />
-					<div className="horizontal-divisor" />
+const Header = () => {
+	const [opened, setOpened] = useState(false);
+
+	return (
+		<>
+			<div className={`menu-mobile mobile ${opened && 'opened'}`}>
+				<h1 className="title">Menu</h1>
+				<div className="menu-item clickEffect">
+					<img src={FileIcon} alt="Icone de instruções de preparo" />
+					<p>Preparo de exames</p>
 				</div>
-				<img alt="Outubro Rosa Logo" className="image-outubro-rosa-logo clickEffect" src={OutubroRosaLogo} />
+				<div className="separator"></div>
+				<div className="menu-item-red clickEffect">
+					<img src={ExitIcon} alt="Icone de sair" />
+					<p>Sair</p>
+				</div>
 			</div>
-			<div className="buttons-container">
-				<Button text="Instruções de preparo" isOutline />
-				<Button text="Sair" iconName="exit" isLink />
-				{/* <Button text="Sair" iconName="exit" isLink disabled /> */}
-			</div>
-			<div className="menu-container">
-				<img alt="Menu Icone" className="clickEffect" src={MenuIcon} />
-			</div>
-		</Container>
-	</header>
-);
+			<header className="fleury-header">
+				<Container>
+					<div className="logos-container">
+						<div className="logo-container">
+							<img alt="Grupo Fleury" className="image-fleury-logo clickEffect" src={FleuryLogo} />
+							<div className="horizontal-divisor" />
+						</div>
+						<img alt="Outubro Rosa Logo" className="image-outubro-rosa-logo clickEffect" src={OutubroRosaLogo} />
+					</div>
+					<div className="buttons-container">
+						<Button text="Instruções de preparo" isOutline />
+						<Button text="Sair" iconName="exit" isLink />
+						{/* <Button text="Sair" iconName="exit" isLink disabled /> */}
+					</div>
+					<div className="menu-container">
+						<img
+							alt="Menu Icone"
+							className="clickEffect"
+							src={MenuIcon}
+							onClick={() => {
+								setOpened(!opened);
+							}}
+						/>
+					</div>
+				</Container>
+			</header>
+		</>
+	);
+};
 
 export default Header;
