b16fa62a7eb9ad92cba38a2fc88a17f9fa338f62
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2023-09-24 19:51:36 -0300

    feat: Add responsive styling to input component
    - Added new CSS file for responsive styling
    - Media query added for screens with max-width of 768px
    - Updated input component to align items, set border radius, border, display, height, padding, and width within the media query

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './input.css';
+import './input-responsive.css';
 
 const Input = ({ caption, placeholder, setValue, type, value }) => (
 	<div className="input-group">
