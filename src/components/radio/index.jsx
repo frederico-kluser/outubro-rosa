834ac18cb1a4c40e34e57a3cfe6a9fbe18603fb5
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2023-09-25 17:59:16 -0300

    feat: Add radio icons and component
    - Added radio-off-icon.svg and radio-on-icon.svg to assets folder
    - Created Radio component in index.jsx
    - Implemented functionality to toggle selected radio option
    - Styled the radio group and individual radios in radio.css

---
+++ src/components/radio/index.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import RadioOn from '../../assets/radio-on-icon.svg';
+import RadioOff from '../../assets/radio-off-icon.svg';
+import './radio.css';
+
+const Radio = ({ texts, onChange }) => {
+	const [selected, setSelected] = useState(0);
+
+	return (
+		<div className="radio-group">
+			{texts.map((text, index) => (
+				<div
+					key={text}
+					className="radio clickEffect"
+					onClick={() => {
+						onChange(index);
+						setSelected(index);
+					}}
+				>
+					<img src={index === selected ? RadioOn : RadioOff} alt="Icone de radio" />
+					<p>{text}</p>
+				</div>
+			))}
+		</div>
+	);
+};
+
+Radio.propTypes = {
+	texts: PropTypes.arrayOf(PropTypes.string).isRequired,
+	onChange: PropTypes.func.isRequired,
+};
+
+export default Radio;
