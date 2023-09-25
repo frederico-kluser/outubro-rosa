42516807f56a907160b3f1b085b9526af9987c74
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2023-09-25 17:59:06 -0300

    feat: Add checkbox component
    - Added new CSS file for the checkbox component
    - Implemented the Checkbox component with text and onChange props
    - Toggled checkbox state on click event

---
+++ src/components/checkbox/index.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import CheckboxOn from '../../assets/checkbox-on-icon.svg';
+import CheckboxOff from '../../assets/checkbox-off-icon.svg';
+import './checkbox.css';
+
+const Checkbox = ({ text, onChange }) => {
+	const [isChecked, setIsChecked] = useState(false);
+
+	return (
+		<div
+			className="checkbox"
+			onClick={() => {
+				onChange(!isChecked);
+				setIsChecked(!isChecked);
+			}}
+		>
+			<img src={isChecked ? CheckboxOn : CheckboxOff} className="clickEffect" alt="Icone de checkbox" />
+			<p>{text}</p>
+		</div>
+	);
+};
+
+Checkbox.propTypes = {
+	text: PropTypes.string.isRequired,
+	onChange: PropTypes.func.isRequired,
+};
+
+export default Checkbox;
