aaafe11cf78f304eba20bf2b65d8b3b3f0b2b243
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2023-09-26 15:48:14 -0300

    feat: Update warning component with new information icons
    - Renamed `information-icon.svg` to `information-green.icon.svg`
    - Updated fill color and position of elements in `information-green.icon.svg`
    - Renamed `information-icon.svg` to `information-purple.icon.svg`
    - Updated import statements in `warning/index.jsx` to use the new icon files
    - Added new prop `color` to the Warning component, allowing for different colored warnings
    - Added CSS classes `.purple` and `.green` with corresponding background and text colors

--- src/components/warning/index.jsx
+++ src/components/warning/index.jsx
@@ -1,17 +1,20 @@
 import PropTypes from 'prop-types';
-import InformationIcon from '../../assets/information-icon.svg';
+import InformationIconPurple from '../../assets/information-purple.icon.svg';
+import InformationIconGreen from '../../assets/information-green.icon.svg';
 import './warning.css';
 import './warning-responsive.css';
 
-const Warning = ({ text }) => (
-	<div className="warning">
-		<img src={InformationIcon} alt="Information Icon" />
+const Warning = ({ text, color = 'purple' }) => (
+	<div className={`warning ${color}`}>
+		{color === 'purple' && <img src={InformationIconPurple} alt="Icone de informação roxo" />}
+		{color === 'green' && <img src={InformationIconGreen} alt="Icone de informação verde" />}
 		{text}
 	</div>
 );
 
 Warning.propTypes = {
 	text: PropTypes.string.isRequired,
+	color: PropTypes.oneOf(['purple', 'green']),
 };
 
 export default Warning;
