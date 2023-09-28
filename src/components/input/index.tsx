b4b99a59391842350de24346ece77accc37bec39
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2023-09-28 08:50:45 -0300

    feat(input): Add active state and improve error handling
    - Added an active state to the input component, which is triggered when the input is focused.
    - Improved error handling by displaying the error message only when there is an error.
    - Updated CSS classes and styles for the active and error states of the input component.
    
    This commit improves the user experience by providing visual feedback when interacting with the input component and enhances error handling.

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -4,28 +4,35 @@ import InfoIcon from '../../assets/info-filled-icon.svg';
 import './input.css';
 import './input-responsive.css';
 
-const Input = ({ info, error, caption, placeholder, setValue, type, value }) => (
-	<div className={`input-group ${error && 'error'} ${info && 'info'}`}>
-		<input
-			className="input"
-			value={value}
-			onChange={(e) => {
-				setValue(e.target.value);
-			}}
-			placeholder={placeholder}
-			type={type}
-		/>
-		{error && <p>{error}</p>}
-		{info && <p>{info}</p>}
-		{info && <img className="info-image" src={InfoIcon} alt="Icone de info" />}
-		{caption && <span className={`span ${info && 'info'}`}>{caption}</span>}
-	</div>
-);
+const Input = ({ error, caption, placeholder, setValue, type, value }) => {
+	const [active, setActive] = React.useState(false);
+
+	return (
+		<div
+			className={`input-group ${active && 'active'} ${error && 'error'}`}
+			onFocus={() => setActive(true)}
+			onBlur={() => setActive(false)}
+		>
+			<input
+				className="input"
+				value={value}
+				onChange={(e) => {
+					setValue(e.target.value);
+				}}
+				placeholder={placeholder}
+				type={type}
+			/>
+			{active && <p>{active}</p>}
+			{error && <p>{error}</p>}
+			{error && <img className="error-image" src={InfoIcon} alt="Icone de error" />}
+			{caption && <span className={`span ${error && 'error'}`}>{caption}</span>}
+		</div>
+	);
+};
 
 Input.propTypes = {
 	caption: PropTypes.string,
 	error: PropTypes.string,
-	info: PropTypes.string,
 	placeholder: PropTypes.string.isRequired,
 	setValue: PropTypes.func.isRequired,
 	type: PropTypes.oneOf(['text', 'number', 'password', 'email', 'phone']).isRequired,
