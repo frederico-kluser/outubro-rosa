ae83a7b74c48d36585a80c2da482a261144e3d5c
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2023-09-30 16:35:26 -0300

    feat: Add confirmation modal component
    - Added a new SVG icon for the confirmation modal
    - Created a new `Modal` component with props for title, paragraph, button text, callback function, and open state
    - Implemented the modal's HTML structure and styling using CSS
    - Conditionally render the modal based on the `showModal` state

---
+++ src/components/modal/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import CircleCheckIcon from '../../assets/circle-check-icon.svg';
+import Button from '../../components/button';
+import './modal.css';
+
+const Modal = ({ title, paragraph, buttonText, callback, open = false }) => {
+	const [showModal, setShowModal] = React.useState(open);
+
+	return showModal ? (
+		<div className="modal-background">
+			<div className="modal">
+				<img src={CircleCheckIcon} alt="Ícone de confirmação" />
+				<div className="modal-text-div">
+					<h1 className="modal-title">{title}</h1>
+					<p className="modal-paragraph">{paragraph}</p>
+				</div>
+				<Button
+					text={buttonText}
+					isCondensed
+					onClick={() => {
+						setShowModal(false);
+						callback();
+					}}
+				/>
+			</div>
+		</div>
+	) : null;
+};
+
+Modal.propTypes = {
+	title: PropTypes.string.isRequired,
+	paragraph: PropTypes.string.isRequired,
+	buttonText: PropTypes.string.isRequired,
+	callback: PropTypes.func.isRequired,
+	open: PropTypes.bool,
+};
+
+export default Modal;
