85c310e8e869fee40aaa77eb9dbae45b7427fd19
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2023-09-30 17:15:53 -0300

    feat: Add react-router-dom package
    - Added "react-router-dom" package to the dependencies in package.json
    - Updated package-lock.json with the new version of "react-router-dom"
    - Imported necessary components from "react-router-dom" in App.jsx
    - Wrapped the routes with BrowserRouter and added a Route for Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import './App.css';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './pages/home';
+import './App.css';
 
 function App() {
 	return (
-		<>
-			<Home />
-		</>
+		<BrowserRouter>
+			<Routes>
+				<Route index element={<Home />} />
+				{/* <Route path="*" element={<NoMatch />} /> */}
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
