af2b117712253bf88c89030634fa391fcadc818b
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2023-10-01 05:45:22 -0300

    feat(button): Add error styling to button component
    This commit adds new CSS styles and prop `isError` to the button component. When `isError` is true, the button will have an outline with a color of #8e24aa and a condensed background of #8e24aa. This allows for easy identification of error buttons.
    
    - Added CSS styles for `.button--outline.button--error` and `.button--condensed.button--error`
    - Modified `Button` component to include `isError` prop
    - Updated button's className based on `isError` prop value

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -2,14 +2,19 @@ import PropTypes from 'prop-types';
 import './button.css';
 import Icon from '../icon';
 
-const Button = ({ id, text, isOutline, isCondensed, isLink, iconName, disabled, onClick }) => {
+const Button = ({ id, text, isOutline, isCondensed, isError, isLink, iconName, disabled, onClick }) => {
 	let className = isOutline ? 'button button--outline' : 'button';
 	className = isLink ? 'button button--link' : className;
 	className = isCondensed ? 'button button--condensed' : className;
 	className = disabled ? `${className} button--disabled` : className;
 
 	return (
-		<button id={id || null} className={`${className} clickEffect`} disabled={disabled} onClick={onClick}>
+		<button
+			id={id || null}
+			className={`${className} ${!!isError && 'button--error'} clickEffect`}
+			disabled={disabled}
+			onClick={onClick}
+		>
 			{iconName && <Icon iconName={iconName} />}
 			{text}
 		</button>
@@ -21,6 +26,7 @@ Button.propTypes = {
 	id: PropTypes.string,
 	isOutline: PropTypes.bool,
 	isCondensed: PropTypes.bool,
+	isError: PropTypes.bool,
 	isLink: PropTypes.bool,
 	iconName: PropTypes.string,
 	disabled: PropTypes.bool,
